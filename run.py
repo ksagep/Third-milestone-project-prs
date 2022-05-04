3956a18e3ac1a218f5b524c291ac4586bd1f2316
ksagep <sandor.kisgyorgy@gmail.com>
2022-05-04 18:47:10 +0000

    Create the intro to the game and give a possibility for player to choose continue the game or not.

---
+++ run.py
@@ -0,0 +1,9 @@
+print("Welcome to my computer game!")
+print("It is the classic paper - rock - scissor game in python environment.")
+
+playing = input("Do you want to play with it?")
+
+if playing != "yes":
+    quit("Thank you and have a nice day!")
+    
+print("Okay, let's play!")
