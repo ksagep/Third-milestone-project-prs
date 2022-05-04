541d300bde020152ce1fed4e28f5605b8eeb4eb9
ksagep <sandor.kisgyorgy@gmail.com>
2022-05-04 19:05:50 +0000

    Create the quit step during the game

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
+import random
+
+"""
+Create the intro to the game and give a possibility for player to choose continue the game or not.
+"""
 print("Welcome to my computer game!")
 print("It is the classic paper - rock - scissor game in python environment.")
 
@@ -7,3 +12,9 @@ if playing != "yes":
     quit("Thank you and have a nice day!")
     
 print("Okay, let's play!")
+
+user_wins = 0
+computer_wins = 0
+
+while True:
+    user_input = input("Type rock, paper, scissor")
